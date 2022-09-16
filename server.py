60d6151be0c7ca7f968535445a582f3dc8bde3bf
hyggebox <asya_apto@mail.ru>
2022-09-16 15:12:44 +0300

    Fix index page encoding

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ async def archive(request):
 
 
 async def handle_index_page(request):
-    async with aiofiles.open('index.html', mode='r') as index_file:
+    async with aiofiles.open('index.html', mode='r', encoding='utf-8') as index_file:
         index_contents = await index_file.read()
     return web.Response(text=index_contents, content_type='text/html')
 
